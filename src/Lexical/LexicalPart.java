19d50061613c7c7248834a07dd7a887ce9e82e23
Mahdi <mahdijafari1379@gmail.com>
2021-10-26 22:45:36 +0330

    Project Started

---
+++ src/Lexical/LexicalPart.java
@@ -0,0 +1,10 @@
+package Lexical;
+
+public class LexicalPart {
+    public static String code;
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
